49e8938414a940c016268c3c9ba8c0925355692a
brusselisidro <brusselisidro@gmail.com>
2017-09-20 23:08:57 +0800

    commit

---
+++ app/src/main/java/com/isidrojacinto/xcube/ace.java
@@ -0,0 +1,13 @@
+package com.isidrojacinto.xcube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ace);
+    }
+}
